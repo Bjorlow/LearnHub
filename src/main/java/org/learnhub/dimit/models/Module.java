8147e996f23714025312384532e009a5ea59d8bc
Bjorlow <dimitritorres34@gmail.com>
2024-04-10 18:26:44 -0500

    Add new module and review classes

---
+++ src/main/java/org/learnhub/dimit/models/Module.java
@@ -0,0 +1,143 @@
+package org.learnhub.dimit.models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+//TODO: Add title
+//TODO: Add description
+//TODO: Add tools - model after skills
+//TODO: Add ingredients - model after skills
+//TODO: Add notes - ArrayList
+//TODO: Add steps - ArrayList
+
+@Entity
+public class Module extends AbstractEntity {
+
+    @ManyToOne
+    @JsonBackReference
+    @JoinColumn(name = "course_Id")
+    private Course course;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    @Column(columnDefinition = "TEXT")
+    private String tools;
+    @Column(columnDefinition = "TEXT")
+    private String ingredients;
+
+    // What category is the main ingredient in it (vegetable, fruit, protein, etc)? Meant to make searching easier.
+//    private String category;
+    @Column(columnDefinition = "TEXT")
+    private String notes;
+    @Column(columnDefinition = "TEXT")
+    private String steps;
+
+    boolean isCompleted;
+
+    public Module() {
+    }
+
+    //TODO: Troubleshoot category -  was breaking on module submission
+    public Module(Course course, String description, String tools, String ingredients, String category, String notes, String steps) {
+        this.course = course;
+        this.description = description;
+        this.tools = tools;
+        this.ingredients = ingredients;
+        this.notes = notes;
+        this.steps = steps;
+    }
+
+    public void finishModule() {
+        this.isCompleted = true;
+    }
+
+    //Getters and setters
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTools() {
+        return tools;
+    }
+
+    public void setTools(String tools) {
+        this.tools = tools;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+//    public String getCategory() {
+//        return category;
+//    }
+//
+//    public void setCategory(String category) {
+//        this.category = category;
+//    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public String getSteps() {
+        return steps;
+    }
+
+    public void setSteps(String steps) {
+        this.steps = steps;
+    }
+
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "Module{" +
+                "id=" + getId() +
+                ", course=" + (course != null ? course.getId() : null) +
+                ", description='" + description + '\'' +
+                ", tools='" + tools + '\'' +
+                ", ingredients='" + ingredients + '\'' +
+                ", notes='" + notes + '\'' +
+                ", steps='" + steps + '\'' +
+                ", isCompleted=" + isCompleted +
+                '}';
+    }
+}
---
+++ src/main/java/org/learnhub/dimit/models/Review.java
@@ -0,0 +1,50 @@
+package org.learnhub.dimit.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class Review extends AbstractEntity {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Comment is required")
+    @Column(columnDefinition = "TEXT")
+    private String comment;
+
+    @NotNull(message = "Rating is required")
+    private Integer rating;
+
+    public Review() {
+
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
